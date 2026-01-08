c16716a752eefa95f31ee6b80eea723b16a2223b
Sandeepramasamy05 <sandeepnageswari33@gmail.com>
2026-01-08 13:45:22 +0530

    1

---
+++ Leetcode/58. Length of Last Word.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def lengthOfLastWord(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        string = s.split()
+        n = len(string)
+        
+        return len(string[n-1])
+        
